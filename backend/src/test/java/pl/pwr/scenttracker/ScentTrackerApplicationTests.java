e4ad903840c6ca05e391499271fedb429df30b51
m-wojtowicz <toz4ist@gmail.com>
2023-04-25 19:42:46 +0200

    Initial commit

---
+++ backend/src/test/java/pl/pwr/scenttracker/ScentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pwr.scenttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
